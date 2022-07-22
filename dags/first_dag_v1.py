dc85c02efca2b5569e5699fb56b91453ef9f46d8
EC2 Default User <ec2-user@ip-172-31-19-151.ec2.internal>
2022-07-22 08:02:37 +0000

    changes

---
+++ dags/first_dag_v1.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+
+
+
+with DAG ('first_dag', start_date=datetime(2022,1,1),
+            schedule_interval='@daily', catchup=False) as dag:
+     create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );'''
+    )
+    
+     create_table
