1aaaacc99e5680279a34cf0815663dcd2c9309e5
EC2 Default User <ec2-user@ip-172-31-19-151.ec2.internal>
2022-08-15 17:25:10 +0000

    adding k8s

---
+++ dags/kubernetes_pods/kubernetes_pods1.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+                                                          KubernetesPodOperator)
+from airflow.configuration import conf
+from airflow.decorators import task
+
+import random
+
+# instantiate the DAG
+with DAG(
+    start_date=datetime(2022,6,1),
+    catchup=False,
+    schedule_interval='@daily',
+    dag_id='Kubernetes_pod_example1'
+) as dag:
+
+
+    transform = KubernetesPodOperator(
+        task_id='transform',
+        image='httpd',
+        in_cluster=True,
+        namespace='airflow',
+        name='my_pod',
+        get_logs=True,
+        log_events_on_failure=True,
+        )
+    
+    transform
