a1a15f13cdfe89ce82743b336d5dff59b76eefc6
EC2 Default User <ec2-user@ip-172-31-19-151.ec2.internal>
2022-08-15 18:06:09 +0000

    adding k8s

--- dags/kubernetes_pods/kubernetes_pods3.py
+++ dags/kubernetes_pods/kubernetes_pods3.py
@@ -23,15 +23,23 @@ with DAG(
         cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
         name="write-xcom",
         do_xcom_push=True,
-        #is_delete_operator_pod=True,
+        is_delete_operator_pod=True,
         in_cluster=True,
         task_id="write-xcom",
         get_logs=True,
     )
     
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"pwd\" && echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\" && sleep 60",
-        task_id="pod_task_xcom_result",
+    read_xcom = KubernetesPodOperator(
+        namespace='airflow',
+        image='alpine',
+        cmds=["sh", "-c", "echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\" && sleep 60"],
+        name="read-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="read-xcom",
+        get_logs=True,
     )
+    
 
-    write_xcom >> pod_task_xcom_result
+    write_xcom >> read_xcom
