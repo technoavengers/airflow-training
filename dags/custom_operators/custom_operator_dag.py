d56c14f7251a57ca7186cc4032658c882d1bc55b
technoavengers <56863046+technoavengers@users.noreply.github.com>
2023-04-27 23:12:29 +0530

    added

--- dags/custom_operators/custom_operator_dag.py
+++ dags/custom_operators/custom_operator_dag.py
@@ -4,5 +4,5 @@ from custom_operators.my_first_operator import MyOperator
 
 
 with DAG(dag_id='custom_operator', schedule='@daily',start_date=datetime(2021, 1, 1),catchup=False) as dag:
- custom_operator = MyOperator(task_id="custom_operator",my_parameter ="sample")
+ custom_operator = MyOperator(task_id="custom_operator",name ="navdeep")
  custom_operator
--- dags/custom_operators/my_first_operator.py
+++ dags/custom_operators/my_first_operator.py
@@ -1,26 +1,14 @@
 # import the operator to inherit from
 from airflow.models.baseoperator import BaseOperator
 
-
-# define the class inheriting from an existing operator class
 class MyOperator(BaseOperator):
-    """
-    Simple example operator that logs one parameter and returns a string saying hi.
-    :param my_parameter: (required) parameter taking any input.
-    """
 
     # define the .__init__() method that runs when the DAG is parsed
-    def __init__(self, my_parameter, *args, **kwargs):
-        # initialize the parent operator
+    def __init__(self, name, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # assign class variables
-        self.my_parameter = my_parameter
+        self.name = name
 
     # define the .execute() method that runs when a task uses this operator.
-    # The Airflow context must always be passed to '.execute()', so make
-    # sure to include the 'context' kwarg.
     def execute(self, context):
-        # write to Airflow task logs
-        self.log.info(self.my_parameter)
-        # the return value of '.execute()' will be pushed to XCom by default
-        return "hi :)"
+        message = f"Hello {self.name}"
+        return message
