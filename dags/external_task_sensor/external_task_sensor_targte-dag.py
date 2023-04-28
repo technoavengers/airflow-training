e3b4b705ebe418bf167da273c63dde92b85c8704
technoavengers <56863046+technoavengers@users.noreply.github.com>
2023-04-28 14:29:09 +0530

    added

--- dags/external_task_sensor/external_task_sensor_targte-dag.py
+++ dags/external_task_sensor/external_task_sensor_targte-dag.py
@@ -5,11 +5,23 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.decorators import task
 from airflow.sensors.external_task import ExternalTaskSensor
 from datetime import timedelta
+from airflow.models import DagRun
+from airflow import AirflowException
 
 default_args = {
     "start_date": airflow.utils.dates.days_ago(1), 
     "owner": "Airflow"
 }
+
+def get_custom_date(execution_date,**kwargs):
+    dag_runs = DagRun.find(dag_id='external_sensor_parent_dag_s3')
+    dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
+    latest_dag_run = dag_runs[0] if dag_runs else None
+    print(dag_runs[0].get_state())
+    if latest_dag_run is not None and latest_dag_run.get_state()== 'success':
+        return latest_dag_run.execution_date
+    else:
+        raise AirflowException("Last DagRun was not successful or not found") 
 
 
 with DAG(dag_id="external_sensor_target_dag", default_args=default_args, schedule_interval="@daily",tags=['external_task_sensor']) as dag:
@@ -18,8 +30,9 @@ with DAG(dag_id="external_sensor_target_dag", default_args=default_args, schedul
     external_task_sensor_task_s3 = ExternalTaskSensor(
         task_id="external_task_sensor_task_s3",
         external_dag_id='external_sensor_parent_dag_s3',
-        external_task_id ='process_file_s3'
+        external_task_id ='process_file_s3',
         #execution_delta = timedelta(minutes=5)
+        execution_date_fn = get_custom_date
     )
 
     external_task_sensor_task_http = ExternalTaskSensor(
