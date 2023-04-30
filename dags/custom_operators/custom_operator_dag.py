b0a657a1970190e30e3e417ecbf9c11d348ebba3
technoavengers <56863046+technoavengers@users.noreply.github.com>
2023-04-30 12:46:45 +0530

    added

--- dags/custom_operators/custom_operator_dag.py
+++ dags/custom_operators/custom_operator_dag.py
@@ -3,9 +3,6 @@ from airflow import DAG
 from custom_operators.my_first_operator import MyOperator
 
 
-# AKIAWDDNL5XOY2Q4K7LP
-# MuL5cjd88vi+DQDjJq7mWf1KYgfaaqDD9c+c9Y0r
-
 with DAG(dag_id='custom_operator', schedule='@daily',start_date=datetime(2021, 1, 1),catchup=False) as dag:
  custom_operator = MyOperator(task_id="custom_operator",name ="navdeep")
  custom_operator
